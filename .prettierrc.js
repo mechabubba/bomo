7be5c6c0dd6eef2dbed468222f8b0860dbfc7bfc
06000208 <52764066+06000208@users.noreply.github.com>
2021-04-26 23:17:23 -0500

    Add prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+// Only used for CSS
+module.exports = {
+    // Two spaces
+    "tabWidth": 2,
+    // Add semicolon after every rule
+    "semi": true,
+    // Use double quotes
+    "singleQuote": false,
+}
