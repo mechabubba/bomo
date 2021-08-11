81459d5b3c5ca2b38116565194e0f87776cc5cd8
06000208 <52764066+06000208@users.noreply.github.com>
2021-08-11 06:35:52 -0500

    Add proper 404 handling & a 404 page

--- index.js
+++ index.js
@@ -63,9 +63,22 @@ db.on("error", err => {
 const { App } = require("@tinyhttp/app");
 const app = new App({
     noMatchHandler: (req, res) => {
-        res.status(404).send({
-            message: "404 Not Found",
-        });
+        res.status(404);
+        // respond with html page
+        if (req.accepts("html")) {
+            res.render("404.ejs", {
+                title: process.env.title,
+                url: req.url,
+            });
+            return;
+        }
+        // respond with json
+        if (req.accepts("json")) {
+            res.json({ error: "Not found" });
+            return;
+        }
+        // default to plain text
+        res.type("txt").send("Not found");
     },
     onError: (err, req, res) => {
         res.status(500).send({
