30e79afe5f64f2c0276d4a6edc0dbf8c477bf065
06000208 <52764066+06000208@users.noreply.github.com>
2021-09-05 01:38:14 -0500

    Update app structure comment in index.js

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
+/** @todo This is gradually going to become outdated, right? Maybe we aught get rid of it */
 /*
 # Tentative Structure
-    public/
-        assets/
-        css/
-        js/
-            main.js - the client code
-    structures/
-        Bomo.js    - internal logic
-        Game.js   - game logic
-        Lobby.js  - lobby class
-        Player.js - a "player"
-    views/
-        _header_, _footer_.ejs - header and footer, visible on every page
-        index.ejs - home thing
-        ???.js - more????
-    index.js - the start of the universe
+    /public/ (Files served static accessible to browsers/client)
+        /assets/
+        /css/
+        /js/ (Client side javascript not using ES6 modules)
+            main.js (unused code, mainly chat stuff, color was moved to modules/structures/)
+        /modules/ (Used for client side ES6 modules)
+            /structures/ (Used for classes)
+                Chat.js
+                Client.js (Client side app)
+                Color.js (unused & unfinished code)
+            index.js (Client side code)
+    /structures/ (Used for classes)
+        Base.js (Reusable abstract class)
+        Bomo.js (Server side app)
+        Game.js (Game logic, completely unfinished)
+        Player.js (Dunno what this is)
+        Room.js (Class representing rooms)
+        User.js (Class representing users)
+    /views/ (Rendered ejs templates)
+        /partials/ (ejs files injected other templates)
+        404.ejs
+        browser.ejs (Unused code, don't touch)
+        index.ejs (Central application, intending on using single page application design)
+        old-index.ejs (Not in the repo, unused old code, don't touch)
+        standard.ejs (Template using the standard partials, may be copy pasted for new pages)
+        test.ejs (Testing page)
+    index.js
 */
 
 const fs = require("fs");
