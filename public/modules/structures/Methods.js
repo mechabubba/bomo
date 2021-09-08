d86b68e12cebfc36e1adf15ee38d4249ee947f3f
06000208 <52764066+06000208@users.noreply.github.com>
2021-09-07 22:48:27 -0500

    Update Methods.js

--- public/modules/structures/Methods.js
+++ public/modules/structures/Methods.js
@@ -6,7 +6,7 @@
  */
 class Methods extends null {
     // ["Content-Type", "application/json"]
-    static async send(method, route = null, options = {}, headers = [], json = true) {
+    static async send(method, route = null, options = {}, headers = [], provideParsed = true) {
         if (!route) throw new TypeError("Valid route required");
         const init = {
             method: method,
@@ -17,23 +17,26 @@ class Methods extends null {
         };
         for (const header of headers) init.headers.append(...header);
         const response = await fetch(new Request(route, init));
-        return json ? await response.json() : response;
+        if (provideParsed) response.parsed = response.headers.get("Content-Type") === "application/json" ? await response.json() || null : null;
+        return response;
     }
-    static async get(route = null, options = {}, headers = [], json = true) {
-        return await Methods.send("GET", route, options, headers, json);
+    static async get(route = null, options = {}, headers = [], provideParsed = true) {
+        return await Methods.send("GET", route, options, headers, provideParsed);
     }
-    static async post(route = null, options = {}, headers = [], json = true) {
-        return await Methods.send("POST", route, options, headers, json);
+    static async post(route = null, options = {}, headers = [], provideParsed = true) {
+        return await Methods.send("POST", route, options, headers, provideParsed);
     }
-    static async put(route = null, options = {}, headers = [], json = true) {
-        return await Methods.send("PUT", route, options, headers, json);
+    static async put(route = null, options = {}, headers = [], provideParsed = true) {
+        return await Methods.send("PUT", route, options, headers, provideParsed);
     }
-    static async delete(route = null, options = {}, headers = [], json = true) {
-        return await Methods.send("DELETE", route, options, headers, json);
+    static async delete(route = null, options = {}, headers = [], provideParsed = true) {
+        return await Methods.send("DELETE", route, options, headers, provideParsed);
     }
-    static async patch(route = null, options = {}, headers = [], json = true) {
-        return await Methods.send("PATCH", route, options, headers, json);
+    static async patch(route = null, options = {}, headers = [], provideParsed = true) {
+        return await Methods.send("PATCH", route, options, headers, provideParsed);
     }
 }
 
 export default Methods;
+// Useful snippet for browser console
+// const { default: Methods } = await import("./modules/structures/Methods.js")
