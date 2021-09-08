bbb27cb888310cec15b6845a8162bb6885ff5834
06000208 <52764066+06000208@users.noreply.github.com>
2021-09-08 00:13:43 -0500

    Update Client.js

--- public/modules/structures/Client.js
+++ public/modules/structures/Client.js
@@ -1,3 +1,4 @@
+import Methods from "./Methods.js";
 import Chat from "./Chat.js";
 
 /**
@@ -13,14 +14,40 @@ class Client extends EventTarget {
          * @see https://developer.mozilla.org/en-US/docs/Web/API/WebSocket
          * @see https://developer.mozilla.org/en-US/docs/Web/API/Location
          */
-        this.ws = new WebSocket(`ws://${document.location.host}/ws`);
+        this.ws = null;
 
-        // Handle websocket messages
+        /**
+         * Unfinished
+         */
+        this.chat = new Chat();
+
+        /**
+         * Session object used with authentication
+         * @property {string} username
+         * @property {string} password
+         * @property {string} encoded
+         * @property {number} expires
+         */
+        this.session = {};
+    }
+
+    /**
+     * Registers a unique session with the server
+     * @param {boolean} cache Whether cached data may be reused
+     */
+    async register(cache = true) {
+        const response = await Methods.post("/api/sessions");
+        if (response.parsed && response.ok) this.session = response.parsed.content;s
+    }
+
+    /**
+     * Establishes stuff websocket connection
+     */
+    async connect() {
+        this.ws = new WebSocket(`ws://${document.location.host}/ws`);
         this.ws.addEventListener("message", function(event) {
             console.log("Message from server ", event.data);
         });
-
-        this.chat = new Chat();
     }
 }
 
