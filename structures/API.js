2cf1cd3ccf7e8714706be7ad996f8eae7ddc4745
06000208 <52764066+06000208@users.noreply.github.com>
2021-09-08 00:12:28 -0500

    Create API.js

---
+++ structures/API.js
@@ -0,0 +1,13 @@
+/**
+ * A container for api route handlers
+ */
+class API extends null {
+    static b(bomo) {
+        //
+    }
+    static a(bomo) {
+        //
+    }
+}
+
+module.exports = API;
