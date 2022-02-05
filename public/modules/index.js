58ecd8591e1da9ff18854572c4a323472247d02c
06000208 <52764066+06000208@users.noreply.github.com>
2022-02-05 03:56:52 -0600

    Reorganize client side & remove jquery

--- public/modules/index.js
+++ public/modules/index.js
@@ -1,5 +1,5 @@
-// import Color from "./structures/Color.js";
-import Client from "./structures/Client.js";
+// import Color from "../structures/Color.js";
+import Client from "../structures/Client.js";
 
 document.addEventListener("DOMContentLoaded", async function(event) {
     const client = new Client();
