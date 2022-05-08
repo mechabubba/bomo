15fdef68e3a07dfed45e37a3dd869d9a710cf6fb
06000208 <52764066+06000208@users.noreply.github.com>
2022-05-08 17:49:26 -0500

    Move the app structure comment to a markdown file

--- index.js
+++ index.js
@@ -1,35 +1,3 @@
-/** @todo Move this to a docs file and update it after the 0.0.3 refactors */
-/*
-# Tentative Structure
-    /public/ (Files served static accessible to browsers/client)
-        /assets/
-        /css/
-        /js/ (Client side javascript not using ES6 modules)
-            main.js (unused code, mainly chat stuff, color was moved to modules/structures/)
-        /modules/ (Used for client side ES6 modules)
-            /structures/ (Used for classes)
-                Chat.js
-                Client.js (Client side app)
-                Color.js (unused & unfinished code)
-            index.js (Client side code)
-    /structures/ (Used for classes)
-        Base.js (Reusable abstract class)
-        Bomo.js (Server side app)
-        Game.js (Game logic, completely unfinished)
-        Player.js (Dunno what this is)
-        Room.js (Class representing rooms)
-        User.js (Class representing users)
-    /views/ (Rendered ejs templates)
-        /partials/ (ejs files injected other templates)
-        404.ejs
-        browser.ejs (Unused code, don't touch)
-        index.ejs (Central application, intending on using single page application design)
-        old-index.ejs (Not in the repo, unused old code, don't touch)
-        standard.ejs (Template using the standard partials, may be copy pasted for new pages)
-        test.ejs (Testing page)
-    index.js
-*/
-
 // loaded asynchronously, order of statements matters
 import "./modules/processEvents.js"; // Node.js process events script
 import "./modules/env.js"; // Populates environment variables script
