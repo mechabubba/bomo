4bc836d23392f49b572b723d3ba62a8a6df9be56
06000208 <52764066+06000208@users.noreply.github.com>
2022-05-08 14:03:09 -0500

    Update id.js

--- modules/id.js
+++ modules/id.js
@@ -1,23 +1,28 @@
 /**
- * Small module for generating unique/collision proof ids inspired by [twitter](https://developer.twitter.com/en/docs/twitter-ids) and [discord](https://discord.com/developers/docs/reference#snowflakes)'s snowflakes.
+ * Small module for generating unique/collision proof ids inspired by [twitter](https://developer.twitter.com/en/docs/twitter-ids)
+ * and [discord](https://discord.com/developers/docs/reference#snowflakes)'s
+ * snowflakes.
  *
- * This doesn't make any attempt to be random or cryptographically secure.
+ * This doesn't make any attempt to be random or cryptographically secure,
+ * rather the opposite. Don't use it for that.
  * @module id
  */
 
-import { DateTime } from "luxon";
-
+/**
+ * Internal number incremented per id generation. Resets to 0 above 65535.
+ * @private
+ */
 let increment = 0;
 
 /**
  * Generates an id
- * @param {DateTime} date
- * @param {number} radix
+ * @param {number} radix An integer in the range `2` through `36` to use with
+ * [toString()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toString)
  * @returns {string}
  */
-const generate = function(date = DateTime.now(), radix = 36) {
+const generateIdentifier = function(radix = 36) {
     if (increment > 65535) increment = 0;
-    return date.toMillis().toString(radix) + (increment++).toString(radix);
+    return Date.now().toString(radix) + (increment++).toString(radix);
 };
 
-export { generate };
+export { generateIdentifier };
