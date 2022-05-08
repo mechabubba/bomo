08b13ea2a878267753458a9d3a8a5f119b69b2a2
06000208 <52764066+06000208@users.noreply.github.com>
2022-05-08 14:45:46 -0500

    Update middlewares

--- modules/middleware.js
+++ modules/middleware.js
@@ -1,31 +1,31 @@
-import { log } from "./log.js";
-import { brandTitle, httpLoggingStyles } from "./constants.js";
+import { log, httpLoggingStyles } from "./log.js";
+import { title } from "./constants.js";
 
 /**
  * @see https://tinyhttp.v1rtl.site/docs#nomatchhandlerreq-res
  */
 const noMatchHandler = (req, res) => {
     res.status(404);
-    // respond with html page
     if (req.accepts("html")) {
-        res.render("404.ejs", {
-            title: `${brandTitle} - 404`,
+        // respond with html page
+        return res.render("template.ejs", {
+            title: `${title} - 404`,
             icon: "/error.ico",
             url: req.url,
+            ...res.locals,
         });
-        return;
-    }
-    // respond with json
-    if (req.accepts("json")) {
-        res.json({
+    } else if (req.accepts("json")) {
+        // respond with json
+        return res.json({
+            "time": Date.now(),
             "code": 404,
             "status": "404 Not Found",
             "message": `The requested resource "${req.url}" was not found`,
         });
-        return;
+    } else {
+        // fallback to plain text
+        return res.type("txt").send("404 Not Found");
     }
-    // default to plain text
-    res.type("txt").send("404 Not Found");
 };
 
 /**
@@ -34,9 +34,10 @@ const noMatchHandler = (req, res) => {
 const onError = (err, req, res) => {
     log.error(req.ip || req.socket.remoteAddress, req.method, req.originalUrl || req.url, err);
     res.status(500).json({
+        "time": Date.now(),
         "code": 500,
         "status": "500 Internal Server Error",
-        "message": err.message,
+        "message": "See logs for more information",
     });
 };
 
@@ -49,8 +50,6 @@ const httpLogger = (req, res, next) => {
         const url = req.originalUrl || req.url;
         const args = [req.ip || req.socket.remoteAddress, req.method, httpLoggingStyles[code[0]](code), res.statusMessage, url];
         /** @todo Some homework would be figuring out how to args.push() the JSON body of requests and responses if present on either */
-        // Not currently using cookies anywhere
-        // if (url === "/") args.push("Cookies:", JSON.stringify(req.cookies));
         const message = args.join(" ").trim();
         if (code[0] === "4" || code[0] === "5") {
             log.debug(message);
