f10d1ac5a146f78d559984aa994012cb03460124
06000208 <52764066+06000208@users.noreply.github.com>
2022-05-08 19:46:15 -0500

    Update templates

--- index.js
+++ index.js
@@ -18,12 +18,16 @@ service.app.get("/api/rooms",
 );
 
 // Route that will always return 200 OK content: true
-service.app.get("/api/test/true", (req, res, next) => res.status(200).json({ content: true }));
+service.app.get("/api/test", (req, res, next) => res.status(200).json({ content: true }));
 
 // Register page routes with tinyhttp
 service.app.get("/", (req, res, next) => res.render("index.ejs", {
     title: "bomo",
     version: version,
+    ...res.locals,
+}));
+service.app.get("/about", (req, res, next) => res.render("about.ejs", {
+    title: "about bomo",
     ...res.locals,
 }));
 
