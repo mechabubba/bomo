619631ec30bcc4d881e64f0394ea173a8767e112
06000208 <52764066+06000208@users.noreply.github.com>
2022-05-08 19:32:50 -0500

    Update start.js

--- modules/start.js
+++ modules/start.js
@@ -4,7 +4,7 @@
  */
 import process from "node:process";
 import { log } from "./modules/log.js";
-import { title, version } from "./modules/constants.js";
+import { version } from "./modules/constants.js";
 
-log.info(`Starting ${title} v${version}, nodejs ${process.versions.node}, on ${process.platform}`);
+log.info(`Starting bomo v${version}, nodejs ${process.versions.node}, on ${process.platform}`);
 if (process.env.dev?.toLowerCase() === "true") log.info("Running in a development environment");
