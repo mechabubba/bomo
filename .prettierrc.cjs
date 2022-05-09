9a194e5824b9d9882313f7b6b5f068aafd8491a3
06000208 <52764066+06000208@users.noreply.github.com>
2022-05-08 19:31:42 -0500

    fix prettier config

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,5 +1,5 @@
-// Only used for CSS
 module.exports = {
+    "printWidth": 280, // Prevents prettier from unnecessarily splitting attributes
     "tabWidth": 4, // Four spaces
     "semi": true, // Add semicolon after every rule
     "singleQuote": false, // Use double quotes
