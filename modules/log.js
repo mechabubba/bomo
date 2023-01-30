98a0b865a905633eacc7482ae07eb91003e442d5
amber <a06000208@protonmail.com>
2023-01-30 07:14:42 -0600

    Update log.js

--- modules/log.js
+++ modules/log.js
@@ -1,18 +1,23 @@
 import pino from "pino";
 import { startTime } from "./constants.js";
 
+const level = "trace";
+
 const log = pino({
-    level: "trace",
+    level: level,
     transport: {
         targets: [
             {
                 target: "pino/file",
-                level: "trace",
+                level: level,
                 options: { destination: `logs/${startTime.toISODate()}_${startTime.toMillis()}.log` },
             },
             {
                 target: "pino-pretty",
-                level: "trace",
+                level: level,
+                options: {
+                    ignore: "pid,hostname",
+                },
             },
         ],
     },
