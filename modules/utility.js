d924704ed73c0e926bf7a4049fd2d918f4d02287
amber <a06000208@protonmail.com>
2023-01-30 07:41:47 -0600

    Create utility.js

---
+++ modules/utility.js
@@ -0,0 +1,10 @@
+/**
+ * String to boolean, where "true" is true and anything else is false
+ *
+ * Mainly useful for environment variables
+ *
+ * For handling number environment variables, use `Number()`
+ * @param {?string|undefined} value
+ * @returns {boolean}
+ */
+export const stringToBoolean = (value) => value && value?.toLowerCase() === "true";
