1f334dfc7e9802b0acdc0b5e5779ed65e6fd569c
amber <a06000208@protonmail.com>
2023-01-30 07:14:22 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,6 @@
 // order of import statements matters, loaded asynchronously
-import "./modules/processEvents.js"; // Node.js process events script
-import "./modules/start.js"; // Starting message
-import "./modules/env.js"; // Populates environment variables script
-import { log } from "./modules/log.js";
-import { service } from "./modules/service.js";
-import { version } from "./modules/constants.js";
+import "./modules/scripts/env.js"; // Populates environment variables
+import "./modules/scripts/processEvents.js"; // Node.js process events
+import "./modules/scripts/startup.js"; // Startup script (version check, logging)
 
-/**
- * Gets room information.
- * @todo https://github.com/mechabubba/bomo/projects/1#card-77107543
- */
-service.app.get("/api/rooms",
-    async (req, res, next) => {
-        const rooms = service.rooms.getRooms();
-        res.status(200).json(rooms);
-    },
-);
-
-// Route that will always return 200 OK content: true
-service.app.get("/api/test", (req, res, next) => res.status(200).json({ content: true }));
-
-// Register page routes with tinyhttp
-service.app.get("/", (req, res, next) => res.render("index.ejs", {
-    title: "bomo",
-    version: version,
-    ...res.locals,
-}));
-service.app.get("/about", (req, res, next) => res.render("about.ejs", {
-    title: "about bomo",
-    ...res.locals,
-}));
-
-// Start
-service.start();
+// import { log } from "./modules/log.js";
