3b1f0d95ff9056f2734c6911fd44a6f63c24deb2
amber <a06000208@protonmail.com>
2023-01-31 06:54:33 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 import "./modules/scripts/env.js"; // Populates environment variables
 import "./modules/scripts/processEvents.js"; // Node.js process events
 import "./modules/scripts/startup.js"; // Startup script (version check, logging)
-
-import { log } from "./modules/log.js";
+import "./modules/scripts/api.js"; // API routes
+import "./modules/scripts/pages.js"; // Page routes
+import { service } from "./modules/service.js";
