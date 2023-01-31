e898202b2f4802814fdad97569c7c3d8044a2aeb
amber <a06000208@protonmail.com>
2023-01-31 00:28:34 -0600

    main modules

--- modules/service.js
+++ modules/service.js
@@ -1,10 +1,8 @@
-import { Service } from "../structures/Service.js";
+import { Service } from "./classes/Service.js";
 import { log } from "./log.js";
 
-const service = new Service({
+export const service = new Service({
     port: process.env.port ? Number(process.env.port) : null,
 });
 
 log.info("Created service");
-
-export { service };
