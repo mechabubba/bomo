9531c936a214ed0b2260783af0a0ed39b35ded4f
amber <a06000208@protonmail.com>
2023-02-07 05:05:47 -0600

    Add route for main page using new template

--- modules/scripts/pages.js
+++ modules/scripts/pages.js
@@ -1,1 +1,10 @@
 import { service } from "../service.js";
+import { version } from "../constants.js";
+
+service.app.get("/", (req, res, next) => res.render("template.ejs", {
+    title: "bomo",
+    // icon: "/assets/example.ico",
+    style: "/css/index.css",
+    partial: "./pages/index.ejs",
+    version: version,
+}));
