79943da3ca3343adb7425f1ec734c3f94396315b
amber <a06000208@protonmail.com>
2023-02-07 05:02:13 -0600

    Clean up Service & add file minification

--- modules/classes/Service.js
+++ modules/classes/Service.js
@@ -1,20 +1,23 @@
+import { env } from "node:process";
 import { join } from "node:path";
 import { Server } from "node:http";
 
 import { App } from "@tinyhttp/app";
-import { cookieParser } from "@tinyhttp/cookie-parser";
+// If we need cookies server side, use this
+// import { cookieParser } from "@tinyhttp/cookie-parser";
 import { json, urlencoded } from "milliparsec";
-import ejs from "ejs";
 import sirv from "sirv";
-import chalk from "chalk";
 
 import { log } from "../log.js";
-import { directory, defaultPort } from "../constants.js";
+import { directory, defaultPort, envFlags } from "../constants.js";
 import { noMatchHandler, onError, requestLogger } from "../middleware.js";
 import { WebSocketManager } from "./WebSocketManager.js";
 import { RoomManager } from "./RoomManager.js";
 import { UserManager } from "./UserManager.js";
 import { GameManager } from "./GameManager.js";
+import { PlayerManager } from "./PlayerManager.js";
+import { stringToBoolean } from "../misc.js";
+import { renderMinifiedFile } from "../ejs.js";
 
 /**
  * @typedef {Object} ServiceOptions
@@ -70,9 +73,14 @@ class Service {
         this.games = new GameManager(this);
 
         /**
-         * Tinyhttp App w/ ejs templating engine
+         * @type {PlayerManager}
+         */
+        this.players = new PlayerManager(this);
+
+        /**
+         * Tinyhttp App w/ eta templating engine
          * @see https://tinyhttp.v1rtl.site/docs#application
-         * @see https://ejs.co/#docs
+         * @see https://eta.js.org/
          * @type {App}
          */
         this.app = new App({
@@ -85,13 +93,13 @@ class Service {
         });
 
         // Engine
-        this.app.engine("ejs", ejs.renderFile);
+        this.app.engine("ejs", renderMinifiedFile);
 
         // Logging middleware
         this.app.use(requestLogger);
 
         // Parse cookie headers via cookie-parser
-        this.app.use(cookieParser());
+        // this.app.use(cookieParser());
 
         // Body parsing via milliparsec
         this.app.use(json());
@@ -100,9 +108,26 @@ class Service {
         // Static webserver using sirv serving the public folder
         // https://www.npmjs.com/package/sirv
         this.app.use("/", sirv(join(directory, "public"), {
-            dev: process.env.dev === "true",
+            dev: stringToBoolean(env[envFlags.dev]),
             maxAge: 86400, // Cached for 24 hours
         }));
+    }
+
+    /**
+     * Shorthand for setting multiple routes at once with the same callback
+     * @param {string} method
+     * @param {string[]} paths
+     * @param {function} callback
+     */
+    route(method, paths, callback) {
+        if (!method) throw new TypeError("method cannot be falsy");
+        if (!paths || !Array.isArray(paths)) throw new TypeError("paths must be an array");
+        if (!callback) throw new TypeError("callback cannot be falsy");
+        if (!paths.length) return;
+        for (const path of paths) {
+            this.app[method](path, callback);
+        }
+        return;
     }
 
     /**
@@ -111,7 +136,7 @@ class Service {
     start() {
         // Create http server and start listening
         this.server = this.app.listen(this.port, () => {
-            log.info(`${chalk.green("[READY]")} Web server listening on port ${this.port}`);
+            log.info(`[READY] Web server listening on port ${this.port}`);
         });
 
         // Create websocket server and add events
---
+++ modules/ejs.js
@@ -0,0 +1,17 @@
+import { renderFile } from "ejs";
+import { minify } from "html-minifier-terser";
+
+/**
+ * Trap for renderFile calls by tinyhttp so we can minify html output
+ * @see https://github.com/tinyhttp/tinyhttp/blob/efd8d92fbd5e752fd9675df54aec8da4b69fce23/packages/app/src/response.ts#L7-L20
+ * @see https://github.com/tinyhttp/tinyhttp/blob/0beb5fe6367a7bffdc989121af7b5e1629bd0f3a/packages/app/src/app.ts#L172
+ */
+export const renderMinifiedFile = function(path, locals, options, callback) {
+    renderFile(path, locals, options, async function(err, html) {
+        if (err) return callback(err, html);
+        const minified = await minify(html, {
+            collapseWhitespace: true,
+        });
+        callback(err, minified);
+    });
+};
