0f34931d150a045debbf7729579f791d214ee9df
amber <a06000208@protonmail.com>
2023-02-07 05:02:32 -0600

    Add test api route

--- modules/scripts/api.js
+++ modules/scripts/api.js
@@ -1,1 +1,4 @@
 import { service } from "../service.js";
+import { contentResponse } from "../responses.js";
+
+service.app.get("/api/true", (req, res, next) => res.status(200).json(contentResponse(true)));
